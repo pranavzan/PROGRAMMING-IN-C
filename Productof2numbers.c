d0abe9164675f0a407a37b29a5daef6dfb1dbce9
pranavzan <pranavzan@gmail.com>
2025-08-14 12:00:47 +0530

    Create Productof3numbers.c
    product of 3 numbers

---
+++ Productof2numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a,b,c,product;
+    printf("Enter A Number\n");
+    scanf("%d", &a);
+    printf("Enter Second Number\n");
+    scanf("%d", &b);
+    printf("Enter Third Number\n");
+    scanf("%d", &c);
+    printf("The Product Of The Number %d and %d and %d is %d \n" , a, b ,c , a*b*c);
+    return 0;
+}
